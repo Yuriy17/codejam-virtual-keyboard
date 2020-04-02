b26b80003a96f0d6231c7966bb467c4306320afc
Yuriy17 <yuriy_sh@zoho.com>
2020-04-02 16:42:34 +0300

    feat: add Textarea class

---
+++ codejam-virtual-keyboard/src/modules/Textarea.js
@@ -0,0 +1,13 @@
+import createElement from '../utils';
+
+export default class Textarea {
+  // eslint-disable-next-line class-methods-use-this
+  init() {}
+
+  // eslint-disable-next-line class-methods-use-this
+  render(rows) {
+    const textarea = createElement('textarea', 'keyboard-input');
+    textarea.setAttribute('rows', rows);
+    return textarea;
+  }
+}
