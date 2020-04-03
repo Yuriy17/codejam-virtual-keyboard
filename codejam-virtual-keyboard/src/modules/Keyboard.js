14c1edd26ee8726efd89878930b03bf444f109a0
Yuriy <yuriy_sh@zoho.com>
2020-04-04 01:43:33 +0300

    feat: realize arrow icon and some fix

--- codejam-virtual-keyboard/src/modules/Keyboard.js
+++ codejam-virtual-keyboard/src/modules/Keyboard.js
@@ -31,45 +31,71 @@ class Keyboard {
 
   _createKeys() {
     const nodeItems = [];
-    /*     const {
-      Backspace, Enter, ShiftRight, IntlBackslash,
-    } = DELIMiTER_KEYS; */
     const { language } = this.properties;
     nodeItems.push(createElement('div', 'keyboard__row'));
 
     this.elements.keys.forEach((element) => {
       const button = createElement('button', 'keyboard__key');
-      nodeItems[nodeItems.length - 1].append(button);
+
       switch (element.type) {
         case 'CONTROL':
           button.innerHTML = element.title;
+          nodeItems[nodeItems.length - 1].append(button);
           if (element.code === 'ShiftRight') {
             button.classList.add('keyboard__key-medium');
             nodeItems.push(createElement('div', 'keyboard__row'));
-          } else if (element.code === 'ShiftRight' || element.code === 'CapsLock') {
+          } else if (element.code === 'ShiftLeft' || element.code === 'CapsLock') {
             button.classList.add('keyboard__key-medium');
           } else {
             button.classList.add('keyboard__key-small');
           }
           break;
+
         case 'NAVIGATION':
-          button.innerHTML = element.title;
           if (element.code === 'Backspace' || element.code === 'Enter') {
+            nodeItems[nodeItems.length - 1].append(button);
             button.classList.add('keyboard__key-large');
+            button.innerHTML = element.title;
             nodeItems.push(createElement('div', 'keyboard__row'));
           } else {
             button.classList.add('keyboard__key-small');
+            let arrow;
+            if (element.code === 'ArrowLeft') {
+              arrow = 'arrow_left';
+              nodeItems[nodeItems.length - 1].append(button);
+            } else if (element.code === 'ArrowRight') {
+              arrow = 'arrow_right';
+              nodeItems[nodeItems.length - 1].append(button);
+            } else if (element.code === 'ArrowUp') {
+              arrow = 'arrow_drop_up';
+              const keyBlock = createElement('div', 'keyboard__key-block');
+              keyBlock.append(button);
+              nodeItems[nodeItems.length - 1].append(keyBlock);
+            } else if (element.code === 'ArrowDown') {
+              arrow = 'arrow_drop_down';
+              nodeItems[nodeItems.length - 1].lastChild.append(button);
+            }
+            const icon = createElement('i', 'material-icons');
+            icon.innerHTML = arrow;
+            button.append(icon);
           }
+
           break;
+
         case 'CHAR':
-          button.innerHTML = element[language].default;
-          if (element.code === 'IntlBackslash') {
+          nodeItems[nodeItems.length - 1].append(button);
+          if (element.code === 'Backslash') {
             nodeItems.push(createElement('div', 'keyboard__row'));
             button.classList.add('keyboard__key-small');
           } else if (element.code === 'Space') {
             button.classList.add('keyboard__key-space');
           } else {
             button.classList.add('keyboard__key-small');
+          }
+          if (element.code === 'Tab') {
+            button.innerHTML = element.title;
+          } else {
+            button.innerHTML = element[language].default;
           }
           break;
 
