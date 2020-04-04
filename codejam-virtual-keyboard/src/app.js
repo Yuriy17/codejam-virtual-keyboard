36e805e1b444ada02375ac2d511d98ba1f526d65
Yuriy17 <yuriy_sh@zoho.com>
2020-04-04 16:02:35 +0300

    docs: add description of keyboard

--- codejam-virtual-keyboard/src/app.js
+++ codejam-virtual-keyboard/src/app.js
@@ -5,3 +5,6 @@ import './favicon.ico';
 const VirtualKeyboard = new App();
 VirtualKeyboard.init('ENGLISH');
 VirtualKeyboard.render(document.body);
+document.body.querySelector('main').insertAdjacentHTML('beforeend',
+  `<br><p>Клавиатура создана в операционной системе Linux</p>
+  <br><p>Для переключения языка комбинация: левыe alt + shift</p>`);
